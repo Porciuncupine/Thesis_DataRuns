613b46472d0a321584d8473f921e86129bbc5e56
Angelo Porciuncula <angelo.porciuncula.school@gmail.com>
2020-10-27 18:16:11 +0800

    includes packages

--- Thesis Code.R
+++ Thesis Code.R
@@ -1,6 +1,24 @@
 ##Thesis Data Set Initial Run
 ##Incomplete Data Set Runs 
 
-#installing necessary packages 
-#loading necessary packages 
+#installing and updating necessary packages 
+  install.packages("stats")
+  install.packages("car")
+  install.paclages("caret")
+  install.paclages("glmnet")
+  install.paclages("sgd")
+  install.paclages("BLR")
+  install.paclages("lars")
+  install.packages("ggplot2")
+  update.packages()
 
+#loading necessary packages 
+  library(stats)
+  library(car)
+  library(caret)
+  library(glmnet)
+  library(sgd)
+  library(BLR)
+  library(lars)
+  library(ggplot2)  
+  
